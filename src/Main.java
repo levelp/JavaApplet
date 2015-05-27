d0f5d75a3d9072c447ac358b3ee988d2004bc922
Denis Stepulenok <super.denis@gmail.com>
2015-05-27 13:39:31 +0300

    Initial state

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("Hello world!", 10, 20);
+    }
+
+}
